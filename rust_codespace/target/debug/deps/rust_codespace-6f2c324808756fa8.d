4236e8eb825e9bebec7989f629d838b3fc1cd652
Juaan00 <145508368+Juaan00@users.noreply.github.com>
2023-12-10 21:37:09 +0000

    yes

---
+++ rust_codespace/target/debug/deps/rust_codespace-6f2c324808756fa8.d
@@ -0,0 +1,5 @@
+/workspaces/PRACTICA_RUST/rust_codespace/target/debug/deps/librust_codespace-6f2c324808756fa8.rmeta: src/main.rs
+
+/workspaces/PRACTICA_RUST/rust_codespace/target/debug/deps/rust_codespace-6f2c324808756fa8.d: src/main.rs
+
+src/main.rs:
---
+++ rust_codespace/target/debug/deps/rust_codespace-79e4116b9143aa3e.d
@@ -0,0 +1,5 @@
+/workspaces/PRACTICA_RUST/rust_codespace/target/debug/deps/librust_codespace-79e4116b9143aa3e.rmeta: src/main.rs
+
+/workspaces/PRACTICA_RUST/rust_codespace/target/debug/deps/rust_codespace-79e4116b9143aa3e.d: src/main.rs
+
+src/main.rs:
