85b039d2812ebc8cdd1d2f2aa1e8bb50b5400d24
Juaan00 <145508368+Juaan00@users.noreply.github.com>
2024-11-07 22:12:42 +0000

    Wolap_12

--- practica_algoritmos/src/main.rs
+++ practica_algoritmos/src/main.rs
@@ -1,17 +1,28 @@
 fn main () {
     // let n: i32 = algoritmo_1(5,10);
     // println!("{}",n);
-    let arr:[i32;5] = [1,3,5,7,9];
-    let arr_2:[i32;5] = [10,9,8,7,6];
-    algoritmo_1(arr,arr_2);
+    // let arr:[i32;5] = [1,3,5,7,9];
+    let arr_2:[i32;9] = [10,7,5,12,20,2,8,13,6];
+    println!("{:?}",arr_2);
+    // algoritmo_1(arr,arr_2);
+    algoritmo_burbuja(arr_2);
+    // println!("{:?}",arr_3);
+    
 }
-fn algoritmo_1(mut dato: [i32;5],dato_2: [i32;5]) {
-    for i in 0..dato.len(){
-        dato[i] +=dato_2[i];
-        println!("{}",dato[i]);   
+// fn algoritmo_1(mut dato: [i32;5],dato_2: [i32;5]) {
+//     for i in 0..dato.len(){
+//         dato[i] +=dato_2[i];
+//         println!("{}",dato[i]);   
+//     }
+// }
+fn algoritmo_burbuja(mut dato: [i32;9]){
+    let ultimo = dato.len();
+    for i in 0..ultimo{
+        if i < ultimo-1 && dato[i] > dato[i+1]{
+            let u = dato[i+1];
+            dato[i + 1] = dato[i];
+            dato[i] = u;
+        }
     }
-
-fn algoritmo_2(dato:[i32;10],objetivo:i32){
-
-}
+    println!("{:?}",dato);
 }
